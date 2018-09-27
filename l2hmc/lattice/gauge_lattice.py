f6e88a02241cdc61988d3aa279a0a5cad93dd608
Sam Foreman <saforem2@gmail.com>
2018-09-27 15:02:19 -0500

    Added option for 'U1' gauge group for link variables.

--- l2hmc/lattice/gauge_lattice.py
+++ l2hmc/lattice/gauge_lattice.py
@@ -18,39 +18,57 @@ EPS = 0.1
 class GaugeLattice(object):
     """Lattice with Gauge field existing on links."""
     def __init__(self, time_size, space_size, dim, beta, link_type):
-
-        if link_type == 'SU2':
-            link_shape = (2, 2)
-
-        if link_type == 'SU3':
-            link_shape = (3, 3)
-
-        if link_type == 'U1':
-            link_shape = (1,)
-
-        self.link_type = link_type
-        self.link_shape = link_shape
-
-        self._sites_shape = tuple([time_size]
-                                  + [space_size for _ in range(dim-1)]
-                                  + list(link_shape))
-
-        self.sites = np.zeros(self._sites_shape, dtype=np.complex64)
-        self.site_idxs = self.sites.shape[:-2]
-
-        self._links_shape = tuple(list(self.site_idxs)
-                                  + [dim]
-                                  + list(link_shape))
-
-        self.links = np.zeros(self._links_shape, dtype=np.complex64)
-        self.link_idxs = self.links.shape[:-2]
-
+        self.time_size = time_size
+        self.space_size = space_size
         self.dim = dim
-        #  self.num_sites = reduce(lambda a, b: a * b, self.site_idxs)
+        self.beta = beta
+        self.link_type = link_type
+
+        self._init_lattice(link_type)
+
         self.num_sites = np.cumproduct(self.site_idxs)[-1]
         self.num_links = self.dim * self.num_sites
-        self.beta = beta
         self.bases = np.eye(dim, dtype=np.int)
+
+    def _init_lattice(self, link_type):
+        """Initialize lattice by creating self.sites and sites.links variables.
+        
+        Link variables are randomly initialized to elements in their respective
+        gauge group.
+        """
+
+        if link_type == 'SU2':
+            self.link_shape = (2, 2)
+
+        if link_type == 'SU3':
+            self.link_shape = (3, 3)
+
+        if link_type == 'U1':
+            self.link_shape = ()
+
+        sites_shape = tuple(
+            [self.time_size]
+            + [self.space_size for _ in range(self.dim-1)]
+            + list(self.link_shape)
+        )
+
+        links_shape = tuple(
+            [self.time_size]
+            + [self.space_size for _ in range(self.dim-1)]
+            + [self.dim]
+            + list(self.link_shape)
+        )
+
+        self.sites = np.zeros(sites_shape, dtype=np.complex64)
+        self.links = np.zeros(links_shape, dtype=np.complex64)
+
+        if self.link_shape != ():
+            # Indices for individual sites and links
+            self.site_idxs = self.sites.shape[:-2]
+            self.link_idxs = self.links.shape[:-2]
+        else:
+            self.site_idxs = self.sites.shape
+            self.link_idxs = self.links.shape
 
         if link_type == 'SU2':
             for link in self.iter_links():
@@ -61,8 +79,12 @@ class GaugeLattice(object):
                                             EPS).reshape(self.links.shape)
 
         if link_type == 'U1':
-            for link in self.iter_links():
-                self.links[link] = 
+            self.links = 2 * np.pi * np.random.rand(*self.link_idxs)
+
+        self.sites_flat = self.sites.flatten()
+        self.links_flat = self.links.flatten()
+
+
 
     def _get_random_links(self, link_type=None):
         """Method for obtaning an array of randomly initialized link variables.
@@ -86,31 +108,21 @@ class GaugeLattice(object):
         if link_type == 'SU3':
             _links = generate_SU3_array(self.num_links // 2,
                                         EPS).reshape(self.links.shape)
+
+        if link_type == 'U1':
+            _links = np.random.rand(*self.links.shape)
+
         return _links
 
     def get_links_samples(self, num_samples, link_type=None):
         """Returns `num_samples` randomly initialized links arrays."""
-        return [self._get_random_links(link_type) for _ in range(num_samples)]
-
-    #  def _random_links(self):
-    #      links = np.zeros(
-    #          list(self.sites.shape) + [self.dim],
-    #          dtype=self.link_type
-    #      )
-    #      for link in self.iter_links():
-    #          links[link] = self.link_type.get_random_element()
-    #      return links
-
-    #  def get_samples(self, num_samples):
-    #      samples = []
-    #      for i in range(num_samples):
-    #          links = np.zeros(self.links.shape, dtype=np.complex)
-    #          for link in self.iter_links():
-    #              links[link] = self.link_type.get_random_element()
-    #          samples.append(links)
-    #      return np.array(samples)
+        samples = [
+            self._get_random_links(link_type) for _ in range(num_samples)
+        ]
+        return np.array(samples)
 
     def iter_sites(self):
+        """Iterator for looping over sites."""
         for i in range(self.num_sites):
             indices = list()
             for dim in self.site_idxs:
@@ -119,11 +131,13 @@ class GaugeLattice(object):
             yield tuple(indices)
 
     def iter_links(self):
+        """Iterator for looping over links."""
         for site in self.iter_sites():
             for mu in range(self.dim):
                 yield tuple(list(site) + [mu])
 
     def get_neighbors(self, site):
+        """Returns neighbors of `site`."""
         shape = self.sites.shape
         neighbors = list()
         for i, dim in enumerate(shape):
@@ -170,63 +184,59 @@ class GaugeLattice(object):
         if links is None:
             links = self.links
 
-        if links.shape[-1] == self.num_links:
-            links = tf.reshape(links, [-1] + list(self.links.shape))
+        if len(links.shape) == 1:
+            links = tf.reshape(links, self.links.shape)
+
+        #  if links.shape[-1] == self.num_links:
+        #      links = tf.reshape(links, [-1] + list(self.links.shape))
 
         S = 0.0
+        if self.link_shape != ():
+            const = 5.0 / 3.0  # constant multiplicative factor
+        else:
+            const = 1.
+
         for site in self.iter_sites():
             for mu in range(self.dim):
                 for nu in range(self.dim):
                     if nu > mu:
-                        S += (5.0 / 3.0) * self.plaquette_operator(links, site,
-                                                                   mu, nu)
-        return S
+                        S += const * self.plaquette_operator(links, site,
+                                                             mu, nu)
+        return np.float32(S)
 
     def total_action(self, batch, batch_size):
         action_arr = []
         for idx in range(batch_size):
             action_arr.append(self._total_action(batch[idx]))
-        return action_arr
+        return np.array(action_arr, dtype=np.float32)
 
     def plaquette_operator(self, links, site, mu, nu):
+        """Local (counter-clockwise) plaquette operator calculated at `site`
+
+        Args:
+            links: Array of link variables (shape = self.links.shape)
+            site: Starting point of plaquette loop calculation.
+            mu: First direction (0 <= mu <= self.dim - 1)
+            nu: Second direction (0 <= nu <= self.dim - 1)
+        """
         shape = self.site_idxs
-        #  site = np.array(site)
-        #  shape = self.link_idxs
-        #  import pdb
-        #  pdb.set_trace()
+
         l1 = tuple(list(np.mod(site, shape)) + [mu])
         l2 = tuple(list(np.mod((site + self.bases[mu]), shape)) + [nu])
         l3 = tuple(list(np.mod((site + self.bases[nu]), shape)) + [mu])
         l4 = tuple(list(np.mod(site, shape)) + [nu])
-        #  if len(l1) == 6 or len(l2) == 6 or len(l3) == 5 or len(l4) == 6:
-        #      import pdb
-        #      pdb.set_trace()
-        #  return 1.0 * np.trace(links[l1]
-        #                        * links[l2]
-        #                        * links[l3].conjugate().T
-        #                        * links[l4].conjugate().T).real / 3.0
-        #  try:
-        #  prod1 = tf.multiply(links[l2], tf.transpose(tf.conj(links[l3])))
-        #  def reshape_link(link):
-        #      return tf.reshape(link, self.link_shape)
-        #
-        #  links[l1] = reshape_link(links[l1])
-        #  links[l2] = reshape_link(links[l2])
-        #  links[l3] = reshape_link(links[l3])
-        #  links[l4] = reshape_link(links[l4])
-        #  prod
-        prod12 = tf.matmul(links[l1], links[l2])
-        prod34 = tf.matmul(tf.transpose(tf.conj(links[l3])),
-                           tf.transpose(tf.conj(links[l4])))
-        prod1234 = tf.matmul(prod12, prod34)
-        try:
+
+        if self.link_shape != ():
+            prod12 = tf.matmul(links[l1], links[l2])
+            prod34 = tf.matmul(tf.transpose(tf.conj(links[l3])),
+                               tf.transpose(tf.conj(links[l4])))
+            prod1234 = tf.matmul(prod12, prod34)
+
             return 1.0 * tf.real(tf.trace(prod1234)) / 3.0
-        except ValueError:
-            import pdb
-            pdb.set_trace()
-        #  except:
-        #      import pdb
-        #      pdb.set_trace()
+
+        else:
+            _sum = links[l1] + links[l2] - links[l3] - links[l4]
+            return np.cos(_sum) / 6.  # each site has 6 plaquettes
 
     def rect_operator(self, links, site, mu, nu):
         shape = self.sites.shape
@@ -237,18 +247,16 @@ class GaugeLattice(object):
         l4 = tuple(list(np.mod((site+self.bases[mu]+self.bases[nu]),shape))+[mu])
         l5 = tuple(list(np.mod((site+self.bases[nu]),shape))+[mu])
         l6 = tuple(list(np.mod(site,shape))+[nu])
-        return 1.0 * tf.real(tf.trace(links[l1]
-                                      * links[l2]
-                                      * links[l3]
-                                      * tf.transpose(tf.conj(links[l4]))
-                                      * tf.transpose(tf.conj(links[l5]))
-                                      * tf.transpose(tf.conj(links[l6]))))
-        #  return 1.0 * np.trace(links[l1]
-        #                        * links[l2]
-        #                        * links[l3]
-        #                        * links[l4].conjugate().T
-        #                        * links[l5].conjugate().T
-        #                        * links[l6].conjugate().T).real
+        if self.link_shape != ():
+            return 1.0 * tf.real(tf.trace(links[l1]
+                                          * links[l2]
+                                          * links[l3]
+                                          * tf.transpose(tf.conj(links[l4]))
+                                          * tf.transpose(tf.conj(links[l5]))
+                                          * tf.transpose(tf.conj(links[l6]))))
+        else:
+            return (links[l1] + links[l2] + links[l3]
+                    - links[l4] - links[l5] - links[l6])
 
     def metropolis_update(self):
         link = self.get_random_link()
