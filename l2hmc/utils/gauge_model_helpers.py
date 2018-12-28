10dec064b5bf15e31f38cabdcbe32b52bde54ad2
Sam Foreman <saforem2@gmail.com>
2018-12-27 23:04:57 -0600

    Fixed helper methods in `utils/gauge_model_helpers.py`

--- l2hmc/utils/gauge_model_helpers.py
+++ l2hmc/utils/gauge_model_helpers.py
@@ -93,68 +93,70 @@ def print_run_data(data, header=False):
     print(data_str)
 
 
-def write_run_data(file_path, data):
+def write_run_data(file_path, data, header=False):
     """Write run `data` to human-readable file at `file_path`."""
     data_str = format_run_data(data)
-    header = ''
+
+    if header:
+        header_str = data_header()
+        with open(file_path, 'a') as f:
+            f.write(header_str)
+            f.write('\n')
+
     step = data['step']
-    if step == 1 or step % 100 == 0:
-        header = data_header(test_flag=True)
+    #  if step == 1 or step % 100 == 0:
+    #      header = data_header(test_flag=True)
     with open(file_path, 'a') as f:
-        f.write(header)
-        f.write('\n')
         f.write(data_str)
         f.write('\n')
-        f.write('avg_plaquettes: {}\n'.format(data['avg_plaquettes']))
-        f.write('topological_charges: {}\n'.format(data['top_charges']))
-        f.write('total_actions: {}\n'.format(data['total_actions']))
-        f.write((len(data_str) + 1)*'-')
+        #  f.write('avg_plaquettes: {}\n'.format(data['avg_plaquettes']))
+        #  f.write('topological_charges: {}\n'.format(data['top_charges']))
+        #  f.write('total_actions: {}\n'.format(data['total_actions']))
+        #  f.write(separator)
 
 
 def write_run_parameters(file_path, parameters):
-    """Write `parameters` to human-readable file at `file_path`."""
+    """Write `parameters` to human-readable file at `file_path`.
+
+    Args:
+        file_path: Path to file to save `parameters` to.
+        parameters (dict)
+    """
     with open(file_path, 'w') as f:
         f.write('Parameters:\n')
         f.write(80 * '-' + '\n')
         for key, val in parameters.items():
-            f.write(f'{key}: {val}\n')
+            if isinstance(val, (int, float, str)):
+                print(f'{key}: {val}\n')
+        #  for key, val in parameters.items():
+        #      f.write(f'{key}: {val}\n')
         f.write(80*'=')
         f.write('\n')
 
 
-def data_header(test_flag=False):
+def data_header():
     """Create formatted (header) string containing labels for printing data."""
-    if test_flag:
-        h_str = ("{:^13s}{:^10s}{:^10s}{:^10s}{:^10s}"
-                 "{:^10s}{:^10s}{:^10s}")
-        h_strf = h_str.format("STEP", "LOSS", "TIME/STEP", "ACCEPT %",
-                              "EPS", "ACTION", "TOP Q", "PLAQ")
-        dash0 = (len(h_strf) + 1) * '='
-        dash1 = (len(h_strf) + 1) * '-'
-        header_str = dash0 + '\n' + h_strf + '\n' + dash1
-
-        return header_str
-
-    h_str = ("{:^13s}{:^10s}{:^10s}{:^10s}{:^10s}"
-             "{:^10s}{:^10s}{:^10s}")
+    h_str = ("{:^13s}{:^12s}{:^12s}{:^12s}{:^12s}{:^12s}") #{:^10s}{:^10s}")
     h_strf = h_str.format("STEP", "LOSS", "TIME/STEP", "ACCEPT %",
-                          "EPS", "ACTION", "TOP Q", "PLAQ")
-    dash = (len(h_strf) + 1) * '-'
-    header_str = dash + '\n' + h_strf + '\n' + dash
+                          "EPS", "LR")#, "ACTION", "TOP Q", "PLAQ")
+    dash0 = (len(h_strf) + 1) * '-'
+    dash1 = (len(h_strf) + 1) * '-'
+    header_str = dash0 + '\n' + h_strf + '\n' + dash1
 
     return header_str
 
 
 def format_run_data(data):
     """Create formatted string containing relevant information from `data`."""
-    data_str = (f"{data['step']:>5g}/{data['train_steps']:<7g}"
-                + f"{data['loss']:^10.4g}"
-                + f"{data['step_time']:^10.4g}"
-                + f"{np.mean(data['accept_prob']):^10.4g}"
-                + f"{data['eps']:^10.4g}"
-                + f"{data['avg_action']:^10.4g}"
-                + f"{data['avg_top_charge']:^10.4g}"
-                + f"{data['avg_plaq']:^10.4g}")
+    data_str = (f"{data['step']:>5g}/{data['train_steps']:<6g} "
+                f"{data['loss']:^11.4g} "
+                f"{data['step_time']:^11.4g} "
+                f"{np.mean(data['accept_prob']):^11.4g} "
+                f"{data['eps']:^11.4g} "
+                f"{data['learning_rate']:^11.4g}")
+                #  + f"{data['avg_action']:^10.4g}"
+                #  + f"{data['avg_top_charge']:^10.4g}"
+                #  + f"{data['avg_plaq']:^10.4g}")
 
     return data_str
 
