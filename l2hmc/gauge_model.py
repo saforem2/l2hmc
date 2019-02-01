8413964ecdea5d301f69a0db55fe4693620de68c
Sam Foreman <saforem2@gmail.com>
2019-02-01 14:05:32 -0600

    Minor update to CLI arguments for gauge_model.

--- l2hmc/gauge_model.py
+++ l2hmc/gauge_model.py
@@ -965,8 +965,8 @@ def main(flags):
         os.environ["KMP_BLOCKTIME"] = str(0)
         os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
         config.allow_soft_placement = True
-        config.intra_op_parallelism_threads = flags.num_intra_threads
-        config.inter_op_parallelism_threads = flags.num_inter_threads
+        #  config.intra_op_parallelism_threads = flags.num_intra_threads
+        #  config.inter_op_parallelism_threads = flags.num_inter_threads
 
 
     model = GaugeModel(params=params,
