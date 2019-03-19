a43d5bc8b7b0d6e6c315833840e5af25236006fb
Sam Foreman <saforem2@gmail.com>
2019-03-19 00:01:06 -0500

    Moved common global variables to `definitions.py`

--- l2hmc/definitions.py
+++ l2hmc/definitions.py
@@ -2,3 +2,11 @@ import os
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(os.getcwd()))
+
+COLORS = 5000 * ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
+MARKERS = 5000 * ['o', 's', 'x', 'v', 'h', '^', 'p', '<', 'd', '>', 'o']
+LINESTYLES = 5000 * ['-', '--', ':', '-.', '-', '--', ':', '-.', '-', '--']
+
+GLOBAL_SEED = 42
